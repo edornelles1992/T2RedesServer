3f3ff08571bf18dc3a058090a328283f794f5321
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2020-05-05 00:09:02 -0300

    ajuste para receber pacotes

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -6,27 +6,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class UDPServer {
+
 	public static void main(String args[]) throws Exception {
-		DatagramSocket serverSocket = new DatagramSocket(5000);
+		DatagramSocket serverSocket = new DatagramSocket(50000);
 
 		byte[] receiveData = new byte[1024];
 		while (true) {
+
 			// declara o pacote a ser recebido
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
 			// recebe o pacote do cliente
 			serverSocket.receive(receivePacket);
 
-			byte[] document = receivePacket.getData();
-
-			try {
-				Path path = Paths.get("arquivoRecebidoUDP" + document.length + ".txt");
-				Files.write(path, document);
-				System.out.println("Arquivo Recebido e salvo com sucesso!");
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("Erro ao salvar arquivo!");
-			}
+			String data = new String(receivePacket.getData());
+			System.out.println("dado recebido: " + data);
 
 		}
 	}
